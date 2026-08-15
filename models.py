79f4a9986a80c257afb93af59f16e1946d8eded4
Vo Dinh An <anp3thetic@gmail.com>
2026-08-15 08:19:36 +0700

    Update models.py

--- models.py
+++ models.py
@@ -3,12 +3,20 @@ from django.conf import settings
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+
+class Instructor(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Learner(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Enrollment(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    content = models.TextField()
 
 class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -21,4 +29,8 @@ class Choice(models.Model):
     is_correct = models.BooleanField(default=False)
 
 class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
+    
+    def is_get_score(self):
+        return 100
